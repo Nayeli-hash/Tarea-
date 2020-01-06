aed9409d6e2ea1276dac9962ee9a9872dc913749
Nayeli-hash <59577303+Nayeli-hash@users.noreply.github.com>
2020-01-06 16:40:06 -0500

    Add files via upload

---
+++ Clase01/Clase01/Calificacion.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase01
+{
+    class Calificacion
+    {
+        //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        //Campos y propiedades
+        //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+        private byte _Codigo;
+        public byte Codigo
+        {
+            get { return _Codigo; }
+            set { _Codigo = value; }
+        }
+
+        private string _Descripcion;
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set { _Descripcion = value; }
+        }
+        
+        private DateTime _FechaHoraRegistro;
+        public DateTime FechaHoraRegistro
+        {
+            get { return _FechaHoraRegistro; }
+            set { _FechaHoraRegistro = value; }
+        }
+
+        private bool _Habilitado;
+        public bool Habilitado
+        {
+            get { return _Habilitado; }
+            set { _Habilitado = value; }
+        }
+
+        private string _NombreBreve;
+        public string NombreBreve
+        {
+            get { return _NombreBreve; }
+            set { _NombreBreve = value; }
+        }
+
+        private string _NombreCompleto;
+        public string NombreCompleto
+        {
+            get { return _NombreCompleto; }
+            set { _NombreCompleto = value; }
+        }
+
+        //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        //Métodos (Acciones)
+        //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        public void Insertar()
+        { 
+        
+        }
+
+        public void Inhabilitar()
+        { 
+        
+        }
+
+        public void Editar(Calificacion argDatos)
+        { 
+            
+        }
+
+        public static Calificacion Buscar_PorCodigo()
+        {
+            Calificacion x = null;
+
+            return x;
+        }
+
+        public static List<Calificacion> Listar()
+        {
+            List<Calificacion> x = new List<Calificacion>();
+
+            return x;
+        }
+
+                
+    }
+}
---
+++ Clase01/Clase01/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Clase01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombreCompleto = new System.Windows.Forms.TextBox();
+            this.txtNombreBreve = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.chbHabilitado = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(497, 52);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 17F);
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de calificación";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label2.Location = new System.Drawing.Point(21, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre completo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label3.Location = new System.Drawing.Point(21, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre breve";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label4.Location = new System.Drawing.Point(21, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Descripción";
+            // 
+            // txtNombreCompleto
+            // 
+            this.txtNombreCompleto.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtNombreCompleto.Location = new System.Drawing.Point(161, 70);
+            this.txtNombreCompleto.Name = "txtNombreCompleto";
+            this.txtNombreCompleto.Size = new System.Drawing.Size(205, 27);
+            this.txtNombreCompleto.TabIndex = 4;
+            // 
+            // txtNombreBreve
+            // 
+            this.txtNombreBreve.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtNombreBreve.Location = new System.Drawing.Point(161, 103);
+            this.txtNombreBreve.Name = "txtNombreBreve";
+            this.txtNombreBreve.Size = new System.Drawing.Size(100, 27);
+            this.txtNombreBreve.TabIndex = 5;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtDescripcion.Location = new System.Drawing.Point(161, 136);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(316, 74);
+            this.txtDescripcion.TabIndex = 6;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btnGuardar.Location = new System.Drawing.Point(326, 272);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(78, 29);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "&Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btnCancelar.Location = new System.Drawing.Point(407, 272);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(78, 29);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // chbHabilitado
+            // 
+            this.chbHabilitado.AutoSize = true;
+            this.chbHabilitado.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.chbHabilitado.Location = new System.Drawing.Point(161, 216);
+            this.chbHabilitado.Name = "chbHabilitado";
+            this.chbHabilitado.Size = new System.Drawing.Size(99, 23);
+            this.chbHabilitado.TabIndex = 9;
+            this.chbHabilitado.Text = "Habilitado";
+            this.chbHabilitado.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGuardar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(497, 313);
+            this.Controls.Add(this.chbHabilitado);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtNombreBreve);
+            this.Controls.Add(this.txtNombreCompleto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Ejemplo 01";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombreCompleto;
+        private System.Windows.Forms.TextBox txtNombreBreve;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.CheckBox chbHabilitado;
+    }
+}
+
